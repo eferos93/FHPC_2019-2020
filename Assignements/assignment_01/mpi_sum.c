1ca07a70c2f2ca47a2c6e7edc3cde3e29480e84e
Eros <eros.fabrici@gmail.com>
2019-11-04 11:46:47 +0100

    ciao

--- Assignements/assignment_01/mpi_sum.c
+++ Assignements/assignment_01/mpi_sum.c
@@ -24,9 +24,8 @@ int main(int argc , char *argv[ ]) {
     */
     long long unsigned int N;
     long long unsigned int local_sum = 0, sum = 0;
-    long long unsigned int array_length = 10;
-    long long unsigned int arr[array_length];
     int myid , numprocs , proc;
+
     MPI_Status status;
     MPI_Request request;
     
@@ -37,8 +36,12 @@ int main(int argc , char *argv[ ]) {
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+    long long unsigned int array_length = 10;
+    long long unsigned int *arr;
     long long unsigned int interval = array_length/numprocs;
-    
+    long long unsigned int index_middle;
+    printf("Interval: %llu ", interval);
+
     for(int i = 0; i<array_length; i++) {
         arr[i] = i;
     }
@@ -48,19 +51,25 @@ int main(int argc , char *argv[ ]) {
         printf("master interval %llu\n", interval);
         unsigned int remainder = array_length % numprocs;
         index = interval + remainder;
-        for(int i = 0; i< array_length; i++) {
-            printf("a[%d] = %llu", i, arr[i]);
-        }
+        index_middle = index*(myid+1) + (array_length - interval)/2;
+        //for(int i = 0; i< array_length; i++) {
+          //  printf("a[%d] = %llu\n", i, arr[i]);
+        //}
         for(int i=0; i< index; i++) {
             sum += arr[i];
         }
         printf("master's sum %llu\n", sum);
+        /*MPI_Send(&index , sizeof(index), MPI_UNSIGNED_LONG_LONG, 1 , tag ,MPI_COMM_WORLD);
+        MPI_Send(&index_middle , sizeof(index_middle), MPI_UNSIGNED_LONG_LONG, 2 , tag ,MPI_COMM_WORLD);
+
+        MPI_Recv(&local_sum, sizeof(local_sum), MPI_UNSIGNED_LONG_LONG, 1, tag, MPI_COMM_WORLD, &status);
+        sum += local_sum;
+        MPI_Recv(&local_sum, sizeof(local_sum), MPI_UNSIGNED_LONG_LONG, 2, tag, MPI_COMM_WORLD, &status);
+        sum += local_sum;*/
+
         for(int proc=1; proc<numprocs; proc++) {
-            index += interval;
-            if(proc == 1)
-                index += remainder;
-            
             MPI_Send(&index , sizeof(index) ,MPI_UNSIGNED_LONG_LONG, proc , tag ,MPI_COMM_WORLD) ;
+            index += interval;
         }
 
         for(int proc=1; proc<numprocs; proc++) {
@@ -71,10 +80,28 @@ int main(int argc , char *argv[ ]) {
         printf("The total sum is %llu\n", sum);
     } else {
         printf("I am proc %i\n", myid);
-        MPI_Recv(&index,sizeof(interval), MPI_UNSIGNED_LONG_LONG, master, tag, MPI_COMM_WORLD, &status);
-        for(index; index<interval*myid; index++) {
-            local_sum += arr[index];
+        MPI_Recv(&index,sizeof(index), MPI_UNSIGNED_LONG_LONG, master, tag, MPI_COMM_WORLD, &status);
+        printf("i have received: %llu\n", index);
+        for(int i = 0; i< array_length; i++) {
+            printf("arr[%d] = %llu", i, arr[i]);
         }
+        /*if(proc*2+1 < numprocs) {
+            for(int i = index; i < interval * myid; i++) {
+                local_sum += arr[i];
+            }
+            index += interval;
+            index_middle = index + 
+
+        } else {
+
+        }*/
+        //long long unsigned int* ptr = &arr;
+        //printf("%p", ptr);
+        //printf("a[%llu] = %llu", index, arr[index]);
+        /*for(int i = index; i<interval*(myid+1); i++) {
+            printf("a[%d] = %llu\n", i, arr[i]);
+            local_sum += arr[i];
+        }*/
         MPI_Send(&local_sum, sizeof(local_sum), MPI_UNSIGNED_LONG_LONG, master, tag, MPI_COMM_WORLD);
     }
 
