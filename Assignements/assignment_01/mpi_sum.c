245f4568813ac7ae7793f79ade0250bfcad71f73
Eros <eros.fabrici@gmail.com>
2019-11-04 21:04:05 +0100

    imporvments

--- Assignements/assignment_01/mpi_sum.c
+++ Assignements/assignment_01/mpi_sum.c
@@ -7,19 +7,12 @@
 
 int main(int argc , char *argv[ ]) {
     
-    FILE* file = fopen ("./input.txt", "r");
-    long long  int array_length = 0;
-
-    fscanf (file, "%llu", &array_length);    
-    while (!feof (file))
-    {  
-      printf ("%llu ", array_length);
-      fscanf (file, "%llu", &array_length);      
-    }
-    fclose (file);        
-    printf("p = %llu", array_length);
     
-    long long  int local_sum = 0, offset;
+    long long  int array_length = 0;
+    double start = 0.0, end = 0.0;
+    
+        
+    long long  int local_sum = 0;
     int myid , numprocs , proc, i;
 
     MPI_Status status;
@@ -31,46 +24,51 @@ int main(int argc , char *argv[ ]) {
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+
+    if(myid == master) { 
+        start = MPI_Wtime();
+        FILE* file = fopen ("./input.txt", "r");
+        fscanf (file, "%llu", &array_length);    
+        while (!feof (file))
+        {  
+            fscanf (file, "%llu", &array_length);      
+        }
+        fclose (file);        
+        end = MPI_Wtime();
+        printf("T_read: %f\n", end-start);
+    }
+    
     long long  int interval = array_length/numprocs;
     int remainder = array_length % numprocs;
 
-    printf("Interval: %llu ", interval);
-
-    
-
     if(myid == master) {
-        printf("master interval %llu\n", interval);
         long long int *sum = (long long int*)malloc(sizeof(sum));
         *sum = 0;
         long long  int *array = (long long  int*)malloc( (interval+remainder) * sizeof(long long  int));
+        start = MPI_Wtime();
         for(i=0; i< interval+remainder; i++) {
             array[i] = i;
             *sum = *sum + array[i];
         }
-        printf("master's sum %llu\n", *sum);
+        end = MPI_Wtime();
+        printf("T_comp: %f\n", end-start);
 
         for(proc=1; proc<numprocs; proc++) {
-            
+            start = MPI_Wtime();
             MPI_Send(&interval , sizeof(interval) ,MPI_LONG_LONG, proc , proc ,MPI_COMM_WORLD);
-            
-            
+            end = MPI_Wtime();
+            printf("T_comm: %f\n", end-start);
         }
 
         for(proc=1; proc<numprocs; proc++) {
             MPI_Recv(&local_sum, sizeof(local_sum), MPI_LONG_LONG, proc, proc, MPI_COMM_WORLD, &status);
-            printf("local sum received %llu\n", local_sum);
-            printf("sum before %llu\n", *sum);
             *sum = *sum + local_sum;
-            printf("sum: %llu\n", *sum);
-
         }
         printf("The total sum is %llu\n", *sum);
     } else {
         MPI_Recv(&interval,sizeof(interval), MPI_LONG_LONG, master, myid, MPI_COMM_WORLD, &status);
 
-        long long int value = interval;
-        printf("proc: %i; i have received: %llu; my remainder: %i\n", myid, interval, remainder);
-        
+        long long int value = interval;        
         long long int *array = (long long int*) malloc(interval*sizeof(long long int));
         for(i= 0; i < interval; i++) {
             array[i] = value*myid+i+remainder;
@@ -79,7 +77,6 @@ int main(int argc , char *argv[ ]) {
         
         for(i = 0; i< interval; i++) {
             local_sum += array[i];
-            printf("arr[%d] = %llu\n", i, array[i]);
         }
 
         MPI_Send(&local_sum, sizeof(local_sum), MPI_UNSIGNED_LONG_LONG, master, myid, MPI_COMM_WORLD);
